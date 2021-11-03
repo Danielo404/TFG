2283f3d61847acfd32db82936f4c68ed7a1ed289
Danielo404 <Danirpalacios@gmail.com>
2021-11-03 19:43:37 +0100

    Creación de modelos

--- src/Models/anotacionModel.java
+++ src/Models/anotacionModel.java
@@ -1,5 +1,64 @@
 package Models;
 
 public class anotacionModel {
+	int idAnotacion;
+	String tipo;
+	String texto;
+	Byte[] archivo;
+	String alumno;
+	String fecha;
+	String hora;
+	
+	public anotacionModel(int pidAnotacion, String ptipo, String ptexto, Byte[] parchivo, String palumno, String pfecha,
+			String phora) {
+		super();
+		idAnotacion = pidAnotacion;
+		tipo = ptipo;
+		texto = ptexto;
+		archivo = parchivo;
+		alumno = palumno;
+		fecha = pfecha;
+		hora = phora;
+	}
+	
+	public anotacionModel(int pidAnotacion, String ptipo, String ptexto, String palumno, String pfecha,
+			String phora) {
+		super();
+		idAnotacion = pidAnotacion;
+		tipo = ptipo;
+		texto = ptexto;
+		alumno = palumno;
+		fecha = pfecha;
+		hora = phora;
+	}
 
+	public int getIdAnotacion() {
+		return idAnotacion;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public Byte[] getArchivo() {
+		return archivo;
+	}
+
+	public String getAlumno() {
+		return alumno;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+	
+	
 }
--- src/Models/profesorModel.java
+++ src/Models/profesorModel.java
@@ -1,5 +1,39 @@
 package Models;
 
 public class profesorModel {
-
+	String nombre;
+	String apellidos;
+	String email;
+	String password;
+	String codigo;
+	String dni;
+	public profesorModel(String pnombre, String papellidos, String pemail, String ppassword, String pcodigo, String pdni) {
+		super();
+		nombre = pnombre;
+		apellidos = papellidos;
+		email = pemail;
+		password = ppassword;
+		codigo = pcodigo;
+		dni = pdni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public String getDni() {
+		return dni;
+	}
+	
+	
 }
